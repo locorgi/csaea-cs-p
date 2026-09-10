193bb174b14692b8582a4005454740ca618f928c
locorgi <mcmahon20308423@student.nhvweb.net>
2026-09-10 16:25:59 +0000

    comic

--- crash-course.py
+++ crash-course.py
@@ -1,11 +1,45 @@
-print ("hello world")
+import random
+import math
+print("hello world")
 # hiiiiiiiii
-a=4  # integer
-b=5.5  # float
-c= "CSAEA" # string
-d= False # boolean
+a = 4  # integer
+b = 5.5  # float
+c = "CSAEA" # string
+d = False # boolean
 print(c)
-print(a,b,c,d)
+print(a, b, c, d)
 # operators + - ? * % ** //
-# conpound operators += -= /=
-e= 11% 10
+# compound operators += -= /=
+e = 11 % 10
+
+# random number generator
+random_number = random.randint(1, 100)
+print("Random number:", random_number)
+#               comparsions  boleans that are alawsy true or false
+# < > <= >= == !=
+print(4<5)
+print(7==4)
+print(1!=2)
+isEqual = "Yes"=="Yes"
+print(isEqual)
+# logical operator
+# in order of precedence not and or
+f= False
+t= True
+print(not f)
+print (f and t)
+print(f or t)
+print(f or t and not f)
+g = int(5.6536539)
+s1 ="Goodnight"
+s2 = " and"
+s3 = " Goodbye"
+end= s1+ s2+ s3
+print(end)
+end += ", Cowboy."
+
+print(end)
+# math libary
+print(math.sqrt(14))
+print(math.ceil(3.65))
+print(math.floor(8.94))
